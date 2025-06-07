35ec3efa6985adf4df8cb449cc3707e59af6a15e
raspadrone <robertfinley@protonmail.com>
2025-06-07 18:53:27 +0200

    grep clap selogic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
 use clap::{Parser, command}; 
 
 #[derive(Parser, Debug)] 
@@ -15,7 +17,31 @@ struct Cli {
     ignore_case: bool,
 }
 
+fn run(config: Cli) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(&config.file_path)?;
+    let reader = BufReader::new(file);
 
+    for (idx, line) in reader.lines().enumerate() {
+        match line {
+            Ok(line_str) => {
+                if config.ignore_case {
+                    if line_str
+                        .to_lowercase()
+                        .contains(&config.query.to_lowercase())
+                    {
+                        println!("Line {}: {line_str}", idx + 1);
+                    }
+                } else {
+                    if line_str.contains(&config.query) {
+                        println!("Line {}: {line_str}", idx + 1);
+                    }
+                }
+            }
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
+    Ok(())
+}
 
 fn main() {
     let cli = Cli::parse(); // This parses arguments, handles errors, and exits if invalid
@@ -24,7 +50,6 @@ fn main() {
     println!("Query: {}", cli.query);
     println!("File Path: {}", cli.file_path);
     println!("Ignore Case: {}", cli.ignore_case);
-
-    // Your existing run function will eventually use these.
-    // For now, just focus on parsing and printing `cli`.
+    let _ = run(cli);
+    
 }
